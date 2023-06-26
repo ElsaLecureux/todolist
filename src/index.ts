7fd2f0b2dc89cdac1c877ca8962e4421541a9799
ElsaLecureux <elsa.lecureux@gmail.com>
2023-06-26 17:21:55 +0200

    adding the code to add tasks on the list

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,58 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { v4 as uuidV4 } from 'uuid';
+//define types of the properties of the object Task
+type Task = {
+  id: string, 
+  title: string, 
+  completed: boolean, 
+  createdAt: Date
+};
+//array task that contains all the tasks of our list
+const tasks : Task[] = [];
+const app = {
+  init () {
+    app.addListenerToForm();
+  },
 
-import confetti from 'canvas-confetti';
+  listElm : document.querySelector<HTMLUListElement>("#list"),
+  formElm : document.getElementById("new-task-form") as HTMLFormElement | null,
+  inputElm : document.querySelector<HTMLInputElement>("#new-task-title"),
+  
+  addListenerToForm () {
+    app.formElm?.addEventListener("submit", e => {
+      e.preventDefault();
+      if (app.inputElm?.value == ""|| app.inputElm?.value == null ) 
+      {
+        return
+      } else {
+        const newTask: Task = {
+          id: uuidV4(), //using uuid to get a new unique id for each task
+          title: app.inputElm?.value,
+          completed: false,
+          createdAt: new Date()
+        };
+        tasks.push(newTask);
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+        app.addListItem(newTask);
+        app.inputElm.value = "";
+      }
+    });
+    
+    
+  },
+
+  addListItem (task: Task) {
+    const itemElm = document.createElement("li");
+    const labelElm = document.createElement("label");
+    const checkboxElm = document.createElement("input");
+    checkboxElm.type = "checkbox";
+    checkboxElm.checked = task.completed;
+    checkboxElm.addEventListener('change', () => {
+      task.completed = checkboxElm.checked;
+    });
+    console.log(task.title);
+    labelElm.append(checkboxElm, task.title);
+    itemElm.append(labelElm);
+    app.listElm?.append(itemElm);
+  }
+}
+document.addEventListener('DOMContentLoaded', app.init );
